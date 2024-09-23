d59555343dc41db37bbb8d3b6bacd67898e69108
Nasywa Althafunnisa <nasywa@student.unp.ac.id>
2024-09-23 18:27:29 +0700

    Add files via upload

---
+++ nomor delapan.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+	char nama[6] = {'N', 'A', 'S', 'Y', 'W', 'A'};
+	for (int i = 0; i < 6; i++) {
+		printf("Huruf ke-%d : %c\n", i, nama[i]);
+	}
+	return 0;
+	
+}
+
